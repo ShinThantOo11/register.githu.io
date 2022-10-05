361506381f0623ae75f6cf7787177cbe5187beda
Shin Thant Oo <shinthantoo.e30@bliss-stock.com>
2022-10-05 14:13:25 +0630

    two

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ upload.addEventListener('change',()=>{
 
 // add_new_category
 let add = document.querySelector('#add_new_category');
-let close = document.querySelectorAll('.close')
+let close = document.querySelectorAll('.close');
 let catBox = document.querySelector('.add-category');
 let inputInSelect = document.querySelector('.add');
 let catTitle = document.querySelector('#new_cat');
@@ -37,11 +37,13 @@ add.addEventListener('click',()=>{
 close.forEach( el => {
     el.addEventListener('click',()=>{
         catBox.classList.add('d-none');
+        delBox.classList.add('d-none');
     })
 });
 
 inputInSelect.addEventListener('click',()=>{
     courseTitle.innerHTML += `<option value="${catTitle.value}">${catTitle.value}</option>`;
+    delteTitle.innerHTML += `<option value="${catTitle.value}">${catTitle.value}</option>`;
     catBox.classList.add('d-none');
     catTitle.value = ''
 })
@@ -49,7 +51,35 @@ inputInSelect.addEventListener('click',()=>{
 catTitle.addEventListener('keyup',(e)=>{
     if(e.key == 'Enter'){
         courseTitle.innerHTML += `<option value="${catTitle.value}">${catTitle.value}</option>`;
+        delteTitle.innerHTML += `<option value="${catTitle.value}">${catTitle.value}</option>`;
         catBox.classList.add('d-none');
         catTitle.value = ''
+    }
+})
+
+// delete category
+
+let del = document.querySelector('#delete_category');
+let delBox = document.querySelector('.delete-category');
+let delBtn = document.querySelector('.deleteBtn');
+let delteTitle = document.querySelector('#delete_cat');
+let courseTitleValue = courseTitle.value;
+
+del.addEventListener('click',()=>{
+    delBox.classList.remove('d-none');
+    delBox.classList.add('ani');
+});
+
+delBtn.addEventListener('click',()=>{
+    if (delteTitle.value != 'Select Category'){
+        for (var i=0; i<courseTitle.length; i++) {
+            if (courseTitle.options[i].value == delteTitle.value)
+                courseTitle.remove(i);
+        }
+        for (var i=0; i<delteTitle.length; i++) {
+            if (delteTitle.options[i].value == delteTitle.value)
+                delteTitle.remove(i);
+        }
+        delBox.classList.add('d-none');
     }
 })
